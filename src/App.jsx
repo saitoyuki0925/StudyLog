bc43937b2851d0a5122fb9bb9fc5a0bcb28cab55
saitoyuki0925 <yuki.free0925@gmail.com>
2025-09-27 18:25:01 +0900

    学習一覧機能の追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+import './App.css';
+import { Archive } from './components/Archive';
+export const App = () => {
+  // テストデータ
+  const records = [
+    { title: '勉強の記録1', time: 1 },
+    { title: '勉強の記録2', time: 3 },
+    { title: '勉強の記録3', time: 5 },
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Archive records={records}></Archive>
     </>
-  )
-}
-
-export default App
+  );
+};
